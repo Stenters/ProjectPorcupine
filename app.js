805904ddf9a167f670ad5a7ff1d4026df2a1a396
Stuart Enters <entersstuart@gmail.com>
2019-08-18 19:46:36 -0500

    adding index.html to repo

--- app.js
+++ app.js
@@ -43,4 +43,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+
+app.set('port', process.env.PORT || 3000)
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port)
+})
