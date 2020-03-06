aac707b105ebd3e82caf9302b5de414375c50e1a
Stuart Enters <enterss@msoe.edu>
2020-03-06 18:05:47 +1300

    more attempts

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ if __name__ == '__main__':
       app.config['SQLALCHEMY_DATABASE_URI'] = \
          'postgres://ylxnjybhzusyza:986c8ebe22636585e47fc3a8ece04a72addf08ff9ff6cf452a865e799de4aed1@ec2-54-225-205-79.compute-1.amazonaws.com:5432/dept28qq0qvuch'
       db = DB.Database(app)
+      print('db created as ' + str(db))
       app.run(debug=False)
